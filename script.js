f174e466dab7f74e895649774e164f0ff7c676cb
Lincoln-Smith <Lincolnsmith628@gmail.com>
2023-12-07 13:42:35 -0500

    it all worked out :D

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ let adverb1 = prompt('Enter an adverb1:')
 
 let story = `Once upon a time, in a ${adjective1} ${noun1}, a ${verb1} ${noun1} ${adverb1} ${verb1}.`
 
-let madLibOutputDiv = document.getElementById()
+let madLibOutputDiv = document.getElementById('madLibOutput')
+
+madLibOutputDiv.innerHTML = `<p> ${story}</p>`
