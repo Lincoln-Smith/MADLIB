776e0bc8c3a06db995997e2aee3996387d065206
Lincoln-Smith <Lincolnsmith628@gmail.com>
2023-12-07 13:35:59 -0500

    help

---
+++ script.js
@@ -0,0 +1,8 @@
+let adjective1 = prompt('Enter an adjective:')
+let noun1 = prompt('Enter an noun1:')
+let verb1 = prompt('Enter an verb1:')
+let adverb1 = prompt('Enter an adverb1:')
+
+let story = `Once upon a time, in a ${adjective1} ${noun1}, a ${verb1} ${noun1} ${adverb1} ${verb1}.`
+
+let madLibOutputDiv = 
