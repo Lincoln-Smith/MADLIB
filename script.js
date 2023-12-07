ff040431d3ab033663f145e4fafcac39b51f1127
Lincoln-Smith <Lincolnsmith628@gmail.com>
2023-12-07 13:42:16 -0500

    AHHHH

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ let adverb1 = prompt('Enter an adverb1:')
 
 let story = `Once upon a time, in a ${adjective1} ${noun1}, a ${verb1} ${noun1} ${adverb1} ${verb1}.`
 
-let madLibOutputDiv = 
+let madLibOutputDiv = document.getElementById()
